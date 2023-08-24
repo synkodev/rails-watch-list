222acca91ac087c460a8063be14d46ff10132d16
Weslley Alencar <weslley.96@outlook.com>
2023-08-24 14:48:12 -0300

    Routes added to the project. Model ok

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,12 @@
 Rails.application.routes.draw do
+  get 'lists/index'
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
   # root "articles#index"
+  resources :lists, only: %i[index show new create] do
+    resources :bookmarks, only: %i[new create]
+  end
+
+  delete 'bookmarks/:id', to: 'bookmarks#destroy'
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,32 @@
 #
 #   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
 #   Character.create(name: "Luke", movie: movies.first)
+
+require 'open-uri'
+require 'json'
+
+puts '==============   SEED BEGIN  =============='
+
+puts 'Step 1: Deleting all the movies from the database'
+
+Movie.destroy_all
+
+puts 'Step 2: Calling the movies API to generate the data'
+
+movies = JSON.parse(URI.open('https://tmdb.lewagon.com/movie/top_rated').read, symbolize_names: true)[:results]
+
+puts "#{movies.count} movies fetched from the API"
+puts 'Step 3: Creating the movies in the database'
+
+# For each movie, insert into the database
+movies.each do |movie|
+  data = Movie.new(title: movie[:title],
+                   overview: movie[:overview],
+                   poster_url: "https://image.tmdb.org/t/p/original#{movie[:poster_path]}",
+                   rating: movie[:vote_average])
+  data.save!
+end
+
+puts "#{Movie.count} movies in the database"
+
+puts '===============   SEED END  =============='
