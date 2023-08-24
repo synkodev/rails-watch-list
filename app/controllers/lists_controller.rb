23c3b4f02c341225e9cc7aeb5548f83087f097c0
Weslley Alencar <weslley.96@outlook.com>
2023-08-24 16:49:22 -0300

    Added list index, show, new, create. Need to implement the other actions

--- app/controllers/lists_controller.rb
+++ app/controllers/lists_controller.rb
@@ -1,4 +1,33 @@
 class ListsController < ApplicationController
+  before_action :set_list, only: %i[show]
+
   def index
+    @lists = List.all
+  end
+
+  def show; end
+
+  def new
+    @list = List.new
+  end
+
+  def create
+    @list = List.new(lists_params)
+
+    if @list.save
+      redirect_to list_path(@list)
+    else
+      render :new, status: :unprocessable_entity
+    end
+  end
+
+  private
+
+  def lists_params
+    params.require(:list).permit(:name)
+  end
+
+  def set_list
+    @list = List.find(params[:id])
   end
 end
